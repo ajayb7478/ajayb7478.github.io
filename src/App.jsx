dbfeda590e577df308b63b8ca262fa9327e4a9bc
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2024-02-09 16:44:35 -0500

    initial/react-tailwind-setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./index.css";
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline mt-120">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
